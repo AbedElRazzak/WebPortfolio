08b4dd054583430a1cdbc3a39d962ff76608f8b6
AbedElRazzak <chreim63@gmail.com>
2023-05-02 14:40:45 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import '../dist/output.css'
+import './input.css'
+
+function App() {
+ 
+
+  return (
+   <div className='h-screen w-full'>
+    <div className='h-[110%] w-full bg-contain bg-no-repeat bg-cover bg-hero-main-desktop'>
+      <div className='h-auto w-full px-4'>
+
+        <div className='flex flex-col mt-16 justify-center items-center'>
+          <div className='h-full'>
+            <div className='h4 h-auto'>Abed</div>
+          </div>
+          <div className='h-[5px] w-[35%] bg-gradient-50'></div>
+            <div className='h4'>El Razzak Chreim</div>
+            <div className='h-[5px] w-[90%] bg-gradient-50'></div>
+        </div>
+
+        <div className='pt-12'>
+          <div className='h-full w-full p2 text-primary-300'>
+            <div className='text-center'>Full stack web developer & a UI/UX Designer, </div>
+            <div className='flex flex-row justify-center'>
+              <div className='text-center pr-2'>currently an intern at </div>
+              <div className='text-center p2 text-gradient-100'>Smartech</div>
+            </div>
+            <div className='text-center'>as a web developer.</div>
+          </div>
+        </div>
+
+
+        
+
+
+      </div>
+
+      <div className='h-auto w-full flex flex-row justify-evenly content-evenly items-center space-x-2 absolute top-[50%]'>
+          <div className='h-32 w-44 bg-contain bg-no-repeat bg-ui-mobile-figma'></div>
+          <div className='h-44 w-28 bg-contain bg-no-repeat bg-ui-mobile-react'></div>
+        </div>
+
+      <div className='h-auto w-full flex justify-center absolute bottom-5'>
+            <div className=' h-auto w-auto flex flex-col space-y-2'> 
+          
+              <div className="inline-block h-12 cursor-pointer rounded-xl border-2 border-primary-400 bg-primary-400 transition delay-[35ms] ease-in hover:border-secondary-300 hover:bg-secondary-300 hover:drop-shadow-2xl">
+                <div className="flex h-full w-full flex-col items-center justify-center rounded-xl transition ease-in hover:bg-secondary-300">
+                  <div className="p1 hover:h6 flex h-12 w-full items-center justify-center rounded-2xl px-8 py-2 text-secondary-300 transition ease-in hover:text-gradient-100 rounded-xl">Connect with me now</div>
+                </div>
+              </div>
+
+              <div className="inline-block h-12 cursor-pointer rounded-xl bg-secondary-200 transition delay-[35ms] ease-in hover:bg-gradient-100 drop-shadow-xl hover:drop-shadow-3xl">
+                <div className="flex h-full w-full flex-col items-center justify-center rounded-xl transition ease-in hover:bg-gradient-100">
+                  <div className="p1 hover:h6 flex h-12 w-full items-center justify-center rounded-2xl px-8 py-2 text-primary-400 transition ease-in hover:text-secondary-100 rounded-xl">Check out my projects</div>
+                </div>
+              </div>
+
+            </div>
+         </div>
+
+
+
+
+
+
+
+
+    </div>
+    
+  </div> 
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,90 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#E6E6E6',
+          100: '#CCCCCC',
+          200: '#999999',
+          300: '#4D4D4D',
+          400: '#020202',
+        },
+        secondary: {
+          50: '#FAFCFE',
+          100: '#F5FBFF',
+          200: '#F6FBFF',
+          300: '#F2F7FB',
+          400: '#DDE2E5',
+        },
+        accent: {
+          blue: {
+            50: "#CCEFFF",
+            100: "#99DFFF",
+            200: "#66CFFF",
+            300: "#35BFFF",
+            400: "#0477E1"
+          },
+          pink: {
+            50: "#E0D5DE",
+            100: "#E0CADD",
+            200: "#E0B3DA",
+            300: "#E09AD6",
+            400: "#E05ACD"
+          },
+          cali: {
+            50: "#EFE8E3",
+            100: "#EFE2D7",
+            200: "#EFD9C6",
+            300: "#EFCCAF",
+            400: "#EFAE78"
+          },
+          purple: {
+            50: "#C0B7D7",
+            100: "#AA96D7",
+            200: "#8461D7",
+            300: "#693BD7",
+            400: "#3F00D7"
+          },
+        },
+        shades: {
+          50: "FFFFFF",
+          100: "000000"
+        },
+        gradient: {
+          50: "",
+          100: ""
+        }
+    },
+    fontSize: {
+      h1: "68.8px",
+      h2: "47.8px",
+      h3: "39.8px",
+      h4: "33.2px",
+      h5: "27.6px",
+      h6: "19.2px",
+      p1: "19.2px",
+      p2: "16px",
+      p3: "13.3px"
+    },
+    fontFamily: {
+      atkinson: ['Atkinson Hyperlegible', 'sans-serif']
+    },
+    dropShadow: {
+      'xl': '0px 0px 16px #BCCFD9',
+      '2xl': '0px 0px 16px #BFD2E1',
+      '3xl': '0px 0px 16px #66CFFF',
+
+    },
+    backgroundImage: {
+      'hero-main-mobile': "url('assets/hero-main-mobile.png')",
+      'hero-main-desktop': "url('assets/hero-main-desktop.png')",
+      'ui-mobile-figma': "url('assets/ui-mobile-figma.png')",
+      'ui-mobile-react': "url('assets/ui-mobile-react.png')"
+    }
+    },
+  },
+  plugins: [],
+}
+
