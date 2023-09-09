83ba11e12539c86cea7863dbb448b3c94c11f32a
AbedElRazzak <chreim63@gmail.com>
2023-09-09 14:40:45 +0300

    hero, intro text updated

--- src/components/HeroMain.jsx
+++ src/components/HeroMain.jsx
@@ -60,8 +60,8 @@ export default function HeroMain(props) {
         
 
 
-      <div className='pt-4'>
-          <div className='h-full w-full p1 text-primary-300 pt-6'>
+      <div className='pt-7'>
+          {/* <div className='h-full w-full p1 text-primary-300 pt-6'>
             {props.lang === "En" ?
             <div className='text-center w-full'>I am a full stack web developer & UI/UX </div>
             :
@@ -71,21 +71,33 @@ export default function HeroMain(props) {
 
               <div className='flex flex-row justify-center'>
                 {props.lang === "En" ?
-                <div className='text-center pr-2'>designer, that blends aesthetics </div>
+                <div className='text-center pr-2'>designer, currently working at </div>
                 :
-                <div className='text-center pr-2'>architect, qui combine l'esthétique</div>
+                <div className='text-center pr-2'>architect, travaillant actuellement à</div>
                 }
                 
                 <div className='text-center p1 text-gradient-100'></div>
               </div>
               {props.lang === "En" ?
-              <div className='text-center'>with functionality.</div>
+              <a href="https://way2godigital.com/" target="/blank" className='text-center font-bold cursor-pointer  text-gradient-50'>Way2GoDigital</a>
               :
-              <div className='text-center'> avec la fonctionnalité.</div>
+              <a href="https://way2godigital.com/" target="/blank" className='text-center font-bold cursor-pointer  text-gradient-50'>Way2GoDigital</a>
               }
               
 
+          </div> */}
+
+          <div className="w-full p1 text-primary-300 flex flex-col justify-center items-center">
+          {props.lang === "En" ?
+            <div className="text-center max-w-xs">I am a full stack web developer & UI/UX designer, currently working at</div>
+            :
+            <div className="text-center max-w-xs">Je suis développeur web full stack et UI/UX architect, travaillant actuellement à</div>
+          }
+            <a href="https://way2godigital.com/" target="/blank" className='text-center font-bold cursor-pointer  text-gradient-100'>Way2GoDigital</a>
+        
           </div>
+
+
         </div>
 
 
